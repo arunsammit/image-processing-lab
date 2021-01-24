3b99467348f9113c94e875b9b9949d5ca362049e
Arun Sammit Pandey <arunsammit@gmail.com>
2021-01-25 02:27:44 +0530

    fixed the problem with reading b/w images

--- assignment-1/main.cpp
+++ assignment-1/main.cpp
@@ -50,8 +50,8 @@ struct color_table{
 class BitMap
 {
 	private:
-		string bm_filename;
 		BMPHeader bm_head;
+		color_table c_table[256];
 		unsigned char*** bm_pixelValues;
     template<typename T>
     void extract(T& to_store ,unsigned char * &header_ptr){
@@ -72,24 +72,53 @@ class BitMap
 			}
       return mat;
     }
+		void init_color_table(){
+			// Populate the color table with corresponding RGB values
+			for (int i = 0; i < 256; i++){
+				c_table[i].B = i;
+				c_table[i].G = i;
+				c_table[i].R = i;
+			}
+		}
 	public:
+		bool isColor;
     //constructor
     BitMap(){}
-    BitMap(string filename){
-      read(filename);
+    BitMap(string fimyImgme){
+      read(fimyImgme);
     }
+		BitMap(const BitMap &bm_cpy){
+			bm_head = bm_cpy.bm_head;
+			isColor = bm_cpy.isColor;
+			if(!isColor){
+				int n = 1<<bm_cpy.bm_head.bit_count;
+				for (size_t i = 0; i < n; i++)
+				{
+					c_table[i] = bm_cpy.c_table[i];
+				}
+				
+			}
+			int third_dimension = isColor?3:1;
+			bm_pixelValues = get3Dmat<unsigned char>(bm_head.height, bm_head.width, third_dimension);
+			for (size_t i = 0; i < bm_head.height; i++)
+				for (size_t j = 0; j < bm_head.width; j++)
+					for (size_t k = 0; k < third_dimension; k++)
+						bm_pixelValues[i][j][k] = bm_cpy.bm_pixelValues[i][j][k];
+		}
+
+		//denotes whether it is color image or not 
 		/**
 		 * A function to read the pixel values and header information
 		 * of the specified .bmp file
 		 *
-		 * @param filename: Path to the .bmp file to be read
+		 * @param fimyImgme: Path to the .bmp file to be read
 		 */
-		void read(string filename){
-			// Store the filename for future reference
-			bm_filename = filename;
+		void read(string fimyImgme){
+			// Store the fimyImgme for future reference
+			// bm_fimyImgme = fimyImgme;
 
 			// Open new binary stream from the input file
-			ifstream bm_stream(filename, ios::binary);
+			ifstream bm_stream(fimyImgme, ios::binary);
 
 			// Store the first 54 bytes (header)
 			unsigned char bm_header[54];
@@ -99,7 +128,6 @@ class BitMap
 			bm_head.file_type[2] = 0;
       bm_stream.read((char *)bm_header,52);
       unsigned char * header_ptr = bm_header; 
-
 			// Store the appropriate fields of file header
 			extract(bm_head.file_size,header_ptr);
       extract(bm_head.reserved1,header_ptr);
@@ -119,6 +147,7 @@ class BitMap
 
 			// This block is for bit-width of 24, i.e., color images
 			if(bm_head.bit_count == 24){
+				isColor = true;
 				// Compute size of data stream in bytes, including padding
 				int dataSize = ((bm_head.width * 3 + 3) & (~3)) * bm_head.height;
         char* img = new char[dataSize];
@@ -140,7 +169,26 @@ class BitMap
 					}
 				}
 			}
+			else{
+				isColor = false;
+				color_table* c_table_ptr = &c_table[0];
+				bm_stream.read((char *)c_table_ptr,sizeof(color_table)*256);
+				int dataSize = ((bm_head.width + 1) & (~1)) * bm_head.height;
+        char* img = new char[dataSize];
+				// Read the pixel values into the array
+				bm_stream.read(img, dataSize);
 
+				// Allocate a 3D array to store the pixel values
+        bm_pixelValues = get3Dmat<unsigned char>(bm_head.height,bm_head.width,1);
+
+				// Copy pixel values to the 3D array in RGB ordering and flipping vertical axis
+				for (int i = 0; i < bm_head.height; i++){
+					for (int j = 0; j < bm_head.width; j++){
+						unsigned char a = img[(bm_head.width*i)+j] & 0xff;
+						bm_pixelValues[bm_head.height-1-i][j][0] = a;	
+					}
+				}
+			}
 			// Close the input stream
 			bm_stream.close();
 		}
@@ -149,11 +197,11 @@ class BitMap
 		 * A function to save the pixel values and header information
 		 * to the specified file
 		 *
-		 * @param filename: Path to the file to be saved
+		 * @param fimyImgme: Path to the file to be saved
 		 */
-		int save(const char* filename){
+		int save(const char* fimyImgme){
 			// Open new output binary stream
-			ofstream bm_stream(filename, ios::binary);
+			ofstream bm_stream(fimyImgme, ios::binary);
 
 			// Write the header information
 			bm_stream << bm_head.file_type[0];
@@ -175,7 +223,7 @@ class BitMap
       write_it(bm_head.colors_imp,bm_stream);
 
 			// Write the pixel values to file for color images
-			if (bm_head.bit_count == 24){
+			if (isColor){
 				int pad;
 				for (int i = 0; i < bm_head.height; i++){
 					for (int j = 0; j < bm_head.width; j++){
@@ -189,30 +237,17 @@ class BitMap
 					for (int i = 0; i < pad; i++)
 						bm_stream << 0x00;
 				}
-			}
-
-			// Write the pixel values to file for grayscale images
-			if (bm_head.bit_count == 8){
-				// Define the color table
-				color_table clr_tbl[256];
-
-				// Populate the color table with corresponding RGB values
-				for (int i = 0; i < 256; i++){
-					clr_tbl[i].B = i;
-					clr_tbl[i].G = i;
-					clr_tbl[i].R = i;
-				}
-
+			}else{
 				// Write the color table to file
-				bm_stream.write((char*)(&clr_tbl), 256*sizeof(color_table));
+				bm_stream.write((char*)(&c_table), 256*sizeof(color_table));
 
 				// Write the pixel values to file
 				for (int i = 0; i < bm_head.height; i++)
 					for (int j = 0; j < bm_head.width; j++){
 						bm_stream << bm_pixelValues[bm_head.height-1-i][j][0];
 					}
-			}
 
+			}
 			// Close the output stream
 			bm_stream.close();
 
@@ -227,23 +262,15 @@ class BitMap
  */
   BitMap flip_bitmap(){
     // Create new empty bitmap for storing output
-    BitMap out;
-
-    // Copy the header information from input
-    out.bm_head = bm_head;
-
-    // Flip the height and width
-    int temp_h = out.bm_head.height;
-    out.bm_head.height = out.bm_head.width;
-    out.bm_head.width = temp_h;
-
+    BitMap out(*this);
+		swap(out.bm_head.height,out.bm_head.width);
+		int third_dimension = out.isColor?3:1;
     // Allocate memory to pixelValues
-    out.bm_pixelValues = get3Dmat<unsigned char>(out.bm_head.height, out.bm_head.width, 3);
-
+		out.bm_pixelValues = get3Dmat<unsigned char>(out.bm_head.height, out.bm_head.width, third_dimension);
     // Flip and copy pixel values
     for (int i = 0; i < out.bm_head.height; i++)
       for (int j = 0; j < out.bm_head.width; j++)
-        for (int k = 0; k < 3; k++)
+        for (int k = 0; k < third_dimension; k++)
           out.bm_pixelValues[i][j][k] = bm_pixelValues[out.bm_head.width - 1 - j][out.bm_head.height - 1 - i][k];
 
     // Return the flipped image
@@ -252,13 +279,12 @@ class BitMap
 
   BitMap bgr_to_gray(string mode){
     // Create new empty bitmap for storing output
-    BitMap out;
-
-    // Copy the header information from input
-    out.bm_head = bm_head;
-
+    BitMap out(*this);
     // Change bit-width of pixels
+		if(!isColor) return out;
     out.bm_head.bit_count = 8;
+		out.init_color_table();
+		out.isColor = false;
 
     // Modify file size
     out.bm_head.file_size = (out.bm_head.height * out.bm_head.width) + sizeof(BMPHeader) + 256*sizeof(color_table);
@@ -272,7 +298,6 @@ class BitMap
     // Allocate memory to pixels
     out.bm_pixelValues = get3Dmat<unsigned char>(out.bm_head.height,out.bm_head.width,1);
     float value;
-
     // Compute grayscale values according to mode and assign them to out
     for (int i = 0; i < out.bm_head.height; i++)
       for (int j = 0; j < out.bm_head.width; j++){
@@ -302,9 +327,6 @@ class BitMap
 		/// Function to display header information in a structured manner
 		void display_header_information(){
 			cout << endl << "HEADER INFORMATION ";
-			// Display the filename, if available
-			if (bm_filename != "")
-				cout << "for file '" << bm_filename << "'";
 			cout << endl;
 			// Separator
 			cout << "__________________________________________________" << endl; 
@@ -358,18 +380,19 @@ int main(int argc, char** argv)
 	}
 
 	// Define and read the .bmp file
-	BitMap lena;
-	lena.read(argv[1]);
-
+	BitMap myImg;
+	myImg.read(argv[1]);
+	myImg.save("./assignment-1/output images/read_img.bmp");
 	// Display the header information
-	lena.display_header_information();
+	myImg.display_header_information();
 
 	// Flip and save the image
-	lena.flip_bitmap().save(argv[2]);
+	myImg.flip_bitmap().save(argv[2]);
 
 	// Convert the image to grayscale and save it
-	lena.bgr_to_gray("avg").save(argv[3]);
-	lena.bgr_to_gray("min").save(argv[4]);
-	lena.bgr_to_gray("max").save(argv[5]);
+	myImg.bgr_to_gray("avg").save(argv[3]);
+	myImg.bgr_to_gray("min").save(argv[4]);
+	myImg.bgr_to_gray("max").save(argv[5]);
+
 	return 0;
 }
