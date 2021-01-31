be7e69cee007338d9cba9d89b214f8173191a933
Arun Sammit Pandey <arunsammit@gmail.com>
2021-01-31 20:05:53 +0530

    restuctured project

---
+++ assignment-2/lib/ImagePath.hpp
@@ -0,0 +1,31 @@
+#include<string>
+#include<iostream>
+#include<filesystem>
+using namespace std;
+
+class ImagePath
+{
+private:
+  string imagePath;
+  string imageName;
+  string imageExtension;
+public:
+  ImagePath(string imagePath);
+  string getImageName();
+  string getExtension();
+};
+
+ImagePath::ImagePath(string imagePath)
+{
+  ImagePath::imagePath = imagePath;
+  stringstream ss1(imagePath);
+  while(getline(ss1,imageName,'/'));
+  imageName = imageName.substr(0,imageName.find('.'));
+  imageExtension = imagePath.substr(imagePath.find('.'));
+}
+string ImagePath::getImageName(){
+  return imageName;
+}
+string ImagePath::getExtension(){
+  return imageExtension;
+}
