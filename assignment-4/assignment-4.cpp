8e1a2830ba5fda8a7a8a8f4ca8525a4a6382257e
arunsammit <arunsammit@gmail.com>
2021-02-22 01:42:08 +0530

    Rewrite of namespace FFT

--- assignment-4/assignment-4.cpp
+++ assignment-4/assignment-4.cpp
@@ -7,124 +7,144 @@
 #include <valarray>
 #include <vector>
 
+#define N 512
+#define pi 3.141592654
+
 using namespace std;
 using namespace std::complex_literals;
-using Complex = std::complex<double>;
+typedef std::complex<double> comp;
 
-const auto N = 512;
-const auto PI = std::acos(-1);
 auto images = std::vector<cv::Mat>();
 int imagePos, filterPos, freqPos;
 
 namespace FFT
 {
-    valarray<Complex> transform(const valarray<Complex> x)
+    vector<vector<comp>> &transpose(vector<vector<comp>> &X)
     {
-        auto n = x.size();
-        auto X = valarray<Complex>(n);
+        for (size_t i = 0; i < N; i++)
+        {
+            for (size_t j = 0; j < i; j++)
+                swap(X[i][j], X[j][i]);
+        }
+        return X;
+    }
+    vector<comp> transform1d(const vector<comp> x)
+    {
+        int n = x.size();
         if (n == 1)
             return x;
-        auto even = FFT::transform(x[slice(0, n / 2, 2)]);
-        auto odd = FFT::transform(x[slice(1, n / 2, 2)]);
-        for (auto k = 0; k != n / 2; k++)
+        vector<comp> X(n);
+        vector<comp> even, odd;
+        for (size_t i = 0; i < n; i++)
         {
-            auto twiddle = exp(-2 * PI * k / n * 1i);
-            X[k] = even[k] + twiddle * odd[k];
-            X[k + n / 2] = even[k] - twiddle * odd[k];
+            even.push_back(x[2*i]);
+            odd.push_back(x[2*i+1]);
+        }
+        for (size_t k = 0; k < n; k++)
+        {
+            X[k] = even[(k % (n / 2))] + exp(-2i * (pi * k / n)) * odd[(k % (n / 2))];
         }
         return X;
     }
 
-    valarray<Complex> inverseTransform(valarray<Complex> X)
+    vector<comp> iTransform1d(vector<comp> X)
     {
-        X = X.apply([](Complex z) { return 1i * conj(z); });
-        auto x = FFT::transform(X);
-        x = x.apply([](Complex z) { return 1i * conj(z); });
-        return x / static_cast<Complex>(x.size());
+        int sz = X.size();
+        vector<comp> ip(sz);
+        for (size_t i = 0; i < X.size(); i++)
+            ip[i] = X[(sz-i)%sz]/((double)sz);
+        vector<comp> op = FFT::transform1d(X);
+        return op;
     }
 
-    valarray<valarray<Complex>> &transpose(valarray<valarray<Complex>> &X)
+    vector<vector<double>> shift2d(const vector<vector<comp>> &X)
     {
-        for (auto i = 0; i != N; ++i)
+        vector<vector<double>> out(N,vector<double>(N));
+        for (size_t i = 0; i < N; i++)
         {
-            for (auto j = 0; j != i; ++j)
+            for (size_t j = 0; j < N; j++)
             {
-                swap(X[i][j], X[j][i]);
+                out[i][j] = log( 1 + abs( X[ (i - N/2 + N)%N ][ (j - N/2 + N)%N ] ) );
             }
         }
-        return X;
+        return out;
     }
 
-    valarray<valarray<Complex>> transform2d(const cv::Mat x)
+
+    vector<vector<comp>> transform2d(vector<vector<comp>> x)
     {
-        auto X = valarray<valarray<Complex>>(valarray<Complex>(N), N);
-        for (auto i = 0; i != N; ++i)
+        for (size_t i = 0; i < N; i++)
         {
-            for (auto j = 0; j != N; ++j)
+            x[i] = FFT::transform1d(x[i]);
+        }
+        for (size_t i = 0; i < N; i++)   
+        {
+            vector<comp> temp(N);
+            for (size_t j = 0; j < N; j++)
             {
-                X[i][j] = static_cast<Complex>(x.at<uint8_t>(i, j));
+                temp[j] = x[i][j]; 
             }
-            X[i] = FFT::transform(X[i]);
+            temp = FFT::transform1d(temp);
+            for (size_t j = 0; j < N; j++)
+            {
+                x[i][j] = temp[j];
+            }
         }
-        X = FFT::transpose(X);
-        for (auto i = 0; i != N; ++i)
-        {
-            X[i] = FFT::transform(X[i]);
-        }
-        return FFT::transpose(X);
+        return x;
+    }
+    vector<vector<comp>> transform2d(cv::Mat x){
+        return transform2d(FFT::toVector(x));
     }
 
-    valarray<valarray<Complex>> shift2d(const valarray<valarray<Complex>> &X)
+    cv::Mat inverseTransform2d(vector<vector<comp>> X)
     {
-        auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
-        for (auto i = 0; i != N; ++i)
+        int si = X.size(),sj = X[0].size(); 
+        vector<vector<comp>> ip(si,vector<comp>(sj));
+        for (size_t i = 0; i < si; i++)
         {
-            for (auto j = 0; j != N; ++j)
+            for (size_t j = 0; j < sj; j++)
             {
-                ret[(N / 2 + i) % N][(N / 2 + j) % N] = log(1 + abs(X[i][j]));
+                ip[i][j] = X[ (si-i)%si ][ (sj-j)%sj ] / (double) (si * sj);
             }
+            
         }
-        return ret;
+        X = FFT::transform2d(X);
+        return FFT::toMat(X);
+        
     }
 
-    cv::Mat inverseTransform2d(valarray<valarray<Complex>> X)
+    cv::Mat toMat(const vector<vector<comp>> &X)
     {
-        auto x = cv::Mat(N, N, CV_8UC1);
-        for (auto i = 0; i != N; ++i)
+        auto x = cv::Mat(X.size(), X[0].size(), CV_8UC1);
+        double max_val;
+        for (size_t i = 0; i < X.size(); i++)
         {
-            for (auto j = 0; j != N; ++j)
+            for (size_t j = 0; j < X[i].size(); j++)
             {
-                X[i][j] = 1i * conj(X[i][j]);
+                max_val = max( X[i][j].real(), max_val);
             }
-            X[i] = FFT::inverseTransform(X[i]);
+            
         }
-        X = FFT::transpose(X);
-        for (auto i = 0; i != N; ++i)
+        
+        for (size_t i = 0; i < X.size(); i++)
         {
-            X[i] = FFT::inverseTransform(X[i]);
-        }
-        X = FFT::transpose(X);
-        for (auto i = 0; i != N; ++i)
-        {
-            for (auto j = 0; j != N; ++j)
+            for (size_t j = 0; j < X[i].size(); j++)
             {
-                x.at<uint8_t>(N - 1 - i, N - 1 - j) = static_cast<uint8_t>(abs(1i * conj(X[i][j])));
+                x.at<int>(i, j) = (255 / max_val) * (X[i][j].real());
             }
         }
         return x;
     }
 
-    cv::Mat toMat(const valarray<valarray<Complex>> &X)
-    {
-        auto x = cv::Mat(N, N, CV_8UC1);
-        for (auto i = 0; i != N; ++i)
+    vector<vector<comp>> toVector(cv::Mat x){
+        vector<vector<comp>> res(x.rows, vector<comp>(x.cols));
+        for (int i = 0; i < x.rows; i++)
         {
-            for (auto j = 0; j != N; ++j)
+            for (int j = 0; j < x.cols; j++)
             {
-                x.at<uint8_t>(i, j) = 255 / 18 * static_cast<uint8_t>(X[i][j].real());
+                res[i][j] = x.at<int>(i,j);
             }
         }
-        return x;
     }
 }
 
@@ -132,21 +152,15 @@ namespace Filter
 {
     namespace LowPass
     {
-        enum _
-        {
-            Ideal = 0,
-            Gaussian,
-            Butterworth,
-        };
 
-        valarray<valarray<Complex>> ideal(const valarray<valarray<Complex>> X, int cutoff)
+        valarray<valarray<comp>> ideal(const valarray<valarray<comp>> X, int cutoff)
         {
-            auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
+            auto ret = valarray<valarray<comp>>(valarray<comp>(N), N);
             for (auto i = 0; i != N; ++i)
             {
                 for (auto j = 0; j != N; ++j)
                 {
-                    if (abs(Complex((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)) > cutoff)
+                    if (abs(comp((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)) > cutoff)
                     {
                         ret[i][j] = 0;
                     }
@@ -159,9 +173,9 @@ namespace Filter
             return ret;
         }
 
-        valarray<valarray<Complex>> gaussian(const valarray<valarray<Complex>> X, int stdDev)
+        valarray<valarray<comp>> gaussian(const valarray<valarray<comp>> X, int stdDev)
         {
-            auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
+            auto ret = valarray<valarray<comp>>(valarray<comp>(N), N);
             for (auto i = 0; i != N; ++i)
             {
                 for (auto j = 0; j != N; ++j)
@@ -173,15 +187,15 @@ namespace Filter
             return ret;
         }
 
-        valarray<valarray<Complex>> butterworth(const valarray<valarray<Complex>> X, int cutoff, int order = 1)
+        valarray<valarray<comp>> butterworth(const valarray<valarray<comp>> X, int cutoff, int order = 1)
         {
-            auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
+            auto ret = valarray<valarray<comp>>(valarray<comp>(N), N);
             for (auto i = 0; i != N; ++i)
             {
                 for (auto j = 0; j != N; ++j)
                 {
                     ret[i][j] = X[i][j] /
-                                (1 + pow(abs(Complex((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)) / cutoff, 2 * order));
+                                (1 + pow(abs(comp((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)) / cutoff, 2 * order));
                 }
             }
             return ret;
@@ -189,21 +203,15 @@ namespace Filter
     };
     namespace HighPass
     {
-        enum _
-        {
-            Ideal = 3,
-            Gaussian,
-            Butterworth,
-        };
 
-        valarray<valarray<Complex>> ideal(const valarray<valarray<Complex>> X, int cutoff)
+        valarray<valarray<comp>> ideal(const valarray<valarray<comp>> X, int cutoff)
         {
-            auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
+            auto ret = valarray<valarray<comp>>(valarray<comp>(N), N);
             for (auto i = 0; i != N; ++i)
             {
                 for (auto j = 0; j != N; ++j)
                 {
-                    if (abs(Complex((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)) > cutoff)
+                    if (abs(comp((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)) > cutoff)
                     {
                         ret[i][j] = X[i][j];
                     }
@@ -216,9 +224,9 @@ namespace Filter
             return ret;
         }
 
-        valarray<valarray<Complex>> gaussian(const valarray<valarray<Complex>> X, int stdDev)
+        valarray<valarray<comp>> gaussian(const valarray<valarray<comp>> X, int stdDev)
         {
-            auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
+            auto ret = valarray<valarray<comp>>(valarray<comp>(N), N);
             for (auto i = 0; i != N; ++i)
             {
                 for (auto j = 0; j != N; ++j)
@@ -230,15 +238,15 @@ namespace Filter
             return ret;
         }
 
-        valarray<valarray<Complex>> butterworth(const valarray<valarray<Complex>> X, int cutoff, int order = 1)
+        valarray<valarray<comp>> butterworth(const valarray<valarray<comp>> X, int cutoff, int order = 1)
         {
-            auto ret = valarray<valarray<Complex>>(valarray<Complex>(N), N);
+            auto ret = valarray<valarray<comp>>(valarray<comp>(N), N);
             for (auto i = 0; i != N; ++i)
             {
                 for (auto j = 0; j != N; ++j)
                 {
                     ret[i][j] = X[i][j] /
-                                (1 + pow(cutoff / abs(Complex((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)), 2 * order));
+                                (1 + pow(cutoff / abs(comp((N / 2 + i) % N - N / 2, (N / 2 + j) % N - N / 2)), 2 * order));
                 }
             }
             return ret;
@@ -253,7 +261,7 @@ static void callBack(int, void *)
     auto inputFFT = FFT::transform2d(input);
 
     auto output = cv::Mat();
-    auto outputFFT = valarray<valarray<Complex>>();
+    auto outputFFT = valarray<valarray<comp>>();
 
     switch (filterPos)
     {
